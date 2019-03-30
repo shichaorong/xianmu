615147d2b61b13d2e39ffd12826b9d7a46bb730d
shichaorong <1447169997@qq.com>
2019-03-30 10:49:01 +0800

    'init'

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+    <routerView/>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style  lang="stylus" rel="stylesheet/stylus" scoped>
+  @import "../static/css/reset.css"
+  html,body
+    width: 100%
+    height: 100%
+    overflow: hidden;
+    .app
+      width: 100%
+      height: 100%
+</style>
---
+++ src/router/routes.js
@@ -0,0 +1,23 @@
+
+import MSite from '../pages/Msite/Msite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+export default [
+  {
+    path: '/msite',
+    component: MSite,
+  },
+  {
+    path: '/search',
+    component: Search,
+  },
+  {
+    path: '/order',
+    component: Order,
+  },
+  {
+    path: '/profile',
+    component: Profile,
+  },
+]
